3ae4d99c2a4f5fa344d46b3ac882977c09ddd41b
Alexander Penkin <sss13594@gmail.com>
2018-07-21 20:32:59 +0500

    heads improved

--- models/gan_cnn_actor.py
+++ models/gan_cnn_actor.py
@@ -204,7 +204,7 @@ class GanCNNActor(Actor):
 
     def log_policy_attention(self, states, head_out):
         states_grad = autograd.grad(
-            head_out.probs.abs().mean() + head_out.state_values.abs().mean(), states,
+            head_out.probs.abs().mean() + head_out.state_value.abs().mean(), states,
             only_inputs=True, retain_graph=True)[0]
         img = states_grad.data[:4]
         img.abs_()
