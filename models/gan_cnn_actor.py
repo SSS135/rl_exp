8397a7bf47aa7de174a83f2b6877121f3068546e
Alexander Penkin <sss13594@gmail.com>
2018-12-09 15:48:44 +0500

    big refactoring

--- models/gan_cnn_actor.py
+++ models/gan_cnn_actor.py
@@ -3,13 +3,13 @@ from itertools import chain
 
 import torch
 from ppo_pytorch.common.make_grid import make_grid
-from ppo_pytorch.models.actors import Actor, ActorOutput
+from ppo_pytorch.models.actors import ModularActor, ActorOutput
 from ppo_pytorch.models.utils import image_to_float, make_conv_heatmap
 from torch import nn as nn, autograd as autograd
 from torch.autograd import Variable
 
 
-class GanCNNActor(Actor):
+class GanCNNActor(ModularActor):
     """
     Convolution network.
     """
