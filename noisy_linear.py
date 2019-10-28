b329f6fd3fbffdff288a74592b8a2216609c6b67
Alexander Penkin <sss13594@gmail.com>
2019-10-28 10:07:56 +0500

    improve NoisyLinear

--- noisy_linear.py
+++ noisy_linear.py
@@ -3,7 +3,6 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 
 
 class NoisyLinear(nn.Module):
@@ -11,41 +10,44 @@ class NoisyLinear(nn.Module):
         super(NoisyLinear, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
+        self.noise_std = noise_std
         self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
+        self.weight_std = nn.Parameter(torch.Tensor(out_features, in_features))
         if bias:
             self.bias = nn.Parameter(torch.Tensor(out_features))
+            self.bias_std = nn.Parameter(torch.Tensor(out_features))
         else:
             self.register_parameter('bias', None)
+            self.register_parameter('bias_std', None)
+
+        self.register_buffer('weight_noise', torch.zeros(self.weight.size()))
+        self.register_buffer('bias_noise', torch.zeros(self.bias.size()) if bias else None)
+
         self.reset_parameters()
-
-        self.register_buffer('noise_w', torch.zeros(self.weight.size()))
-        self.noise_w_std = nn.Parameter(noise_std * torch.ones(self.weight.size()))
-        if bias:
-            self.register_buffer('noise_b', torch.zeros(self.bias.size()))
-            self.noise_b_std = nn.Parameter(noise_std * torch.ones(self.bias.size()))
-        else:
-            self.register_parameter('noise_b_std', None)
-            self.register_buffer('noise_b', None)
-
-        self.reset_noise()
+        self.randomize_noise()
 
     def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.weight.size(1))
-        self.weight.data.uniform_(-stdv, stdv)
-        if self.bias is not None:
-            self.bias.data.uniform_(-stdv, stdv)
+        with torch.no_grad():
+            std = math.sqrt(3 / self.weight.size(1))
+            self.weight.uniform_(-std, std)
+            self.weight_std.fill_(self.noise_std)
+            if self.bias is not None:
+                self.bias.zero_()
+                self.bias_std.fill_(self.noise_std)
 
-    def reset_noise(self):
-        self.noise_w.normal_()
-        if self.bias is not None:
-            self.noise_b.normal_()
+    def randomize_noise(self):
+        with torch.no_grad():
+            self.weight_noise.normal_()
+            if self.bias is not None:
+                self.bias_noise.normal_()
 
     def forward(self, input):
-        weight = self.weight + self.noise_w_std * Variable(self.noise_w)
-        bias = self.bias + self.noise_b_std * Variable(self.noise_b) if self.bias is not None else None
+        weight = self.weight + self.weight_std * self.weight_noise
+        bias = self.bias + self.bias_std * self.bias_noise if self.bias is not None else None
         return F.linear(input, weight, bias)
 
     def __repr__(self):
-        return self.__class__.__name__ + ' (' \
-            + str(self.in_features) + ' -> ' \
-            + str(self.out_features) + ')'
+        return f'{self.__class__.__name__}(' \
+            f'in_features={self.in_features}, ' \
+            f'out_features={self.out_features}, ' \
+            f'bias={self.bias is not None})'
