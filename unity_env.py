40f9f18d7aeaac28065559b9d5163cb7717579cc
Alexander Penkin <sss13594@gmail.com>
2019-11-05 21:25:32 +0500

    support unity multiagent envs

--- unity_env.py
+++ unity_env.py
@@ -2,45 +2,47 @@ import os
 import random
 import time
 from functools import partial
+from multiprocessing.dummy import Pool
 
 import gym
 import gym.spaces
 import numpy as np
+from ppo_pytorch.common.multiplayer_env import MultiplayerEnv
 from mlagents.envs.exception import UnityWorkerInUseException
 from ppo_pytorch.common.env_factory import NamedVecEnv, SimpleVecEnv, Monitor, ObservationNorm
 from mlagents.envs.environment import UnityEnvironment, BrainInfo, BrainParameters, AllBrainInfo
 
 
-class UnityEnv(gym.Env):
+class UnityEnv(MultiplayerEnv):
     def __init__(self, env_name, *args, **kwargs):
         self.env = UnityEnvironment(env_name, *args, base_port=10000, **kwargs)
-        self.env.reset()
-        assert self.env.number_external_brains == 1, self.env.number_external_brains
-        brain_params = self.env.brains[self.env.external_brain_names[0]]
+        brain = next(iter(self.env.reset().values()))
+        assert self.env.number_external_brains == 1
+        brain_params = self.env.external_brains[self.env.external_brain_names[0]]
         self.observation_space = self._get_observation_space(brain_params)
         self.action_space = self._get_action_space(brain_params)
         self.reward_range = (-1, 1)
+        super().__init__(len(brain.agents))
 
     def step(self, action, *args, **kwargs):
         state = self.env.step(self._process_action(action), *args, **kwargs)
-        return self._next(state)
+        return self._process_brain_info(state)
 
     def reset(self, *args, **kwargs):
         state = self.env.reset(*args, **kwargs)
-        return self._next(state)[0]
+        return self._process_brain_info(state)
 
     def __enter__(self):
-        pass
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
 
-    def _next(self, env_state: AllBrainInfo):
+    def _process_brain_info(self, env_state: AllBrainInfo):
         assert len(env_state) == 1, env_state
         brain: BrainInfo = next(iter(env_state.values()))
-        assert len(brain.agents) == 1, brain.agents
-        states = brain.vector_observations
-        return self._process_state(states[0]), brain.rewards[0], brain.local_done[0], {}
+        states = [self._process_state(s) for s in brain.vector_observations]
+        return states, brain.rewards, brain.local_done, [{}] * self.num_players
 
     def _get_action_space(self, brain_params: BrainParameters):
         assert len(brain_params.vector_action_space_size) == 1
@@ -66,13 +68,24 @@ class UnityEnv(gym.Env):
         self.env.close()
 
 
-class UnityVecEnv(SimpleVecEnv):
+class UnityVecEnv(NamedVecEnv):
     def __init__(self, env_path, observation_norm=False, parallel='thread'):
         self.env_path = env_path
         env_name = os.path.basename(os.path.split(os.path.normpath(env_path))[0])
-        super().__init__(env_name, observation_norm, parallel)
+        self.observation_norm = observation_norm
+        self.env_name = env_name
+        self.parallel = parallel
+        self.envs = None
+        self.num_envs = None
+        self.pool = None
 
-    def get_env_fn(self):
+        env: UnityEnv = self.get_env_factory()()
+        self.observation_space = env.observation_space
+        self.action_space = env.action_space
+        self.num_players = env.num_players
+        env.close()
+
+    def get_env_factory(self):
         def make(env_path, observation_norm):
             while True:
                 worker_id = random.randrange(50000)
@@ -88,3 +101,31 @@ class UnityVecEnv(SimpleVecEnv):
 
         return partial(make, self.env_path, self.observation_norm)
 
+    def set_num_envs(self, num_envs):
+        assert num_envs % self.num_players == 0, (num_envs, self.num_players)
+        if self.envs is not None:
+            for env in self.envs:
+                env.close()
+        self.num_envs = num_envs
+        env_factory = self.get_env_factory()
+        self.envs = [env_factory() for _ in range(num_envs // self.num_players)]
+        # self.pool = Pool(self.num_envs // self.num_players)
+
+    def step(self, actions):
+        actions = np.split(np.asarray(actions), len(self.envs))
+        return self._cat_data(env.step(a) for env, a in zip(self.envs, actions))
+
+    def reset(self):
+        return self._cat_data(env.reset() for env in self.envs)[0]
+
+    def _cat_data(self, data):
+        return [np.concatenate(x, 0) for x in zip(*data)]
+
+    # def __enter__(self):
+    #     self.pool.__enter__()
+    #     return self
+    #
+    # def __exit__(self, exc_type, exc_val, exc_tb):
+    #     self.pool.__exit__()
+    #     for env in self.envs:
+    #         env.close()
