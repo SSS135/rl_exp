0883a7b84206f814885e55bc4cf5274d2d1de02d
Alexander Penkin <sss13594@gmail.com>
2019-11-06 13:01:36 +0500

    support visual observations in UnityEnv

--- unity_env.py
+++ unity_env.py
@@ -9,18 +9,19 @@ import gym.spaces
 import numpy as np
 from ppo_pytorch.common.multiplayer_env import MultiplayerEnv
 from mlagents.envs.exception import UnityWorkerInUseException
-from ppo_pytorch.common.env_factory import NamedVecEnv, SimpleVecEnv, Monitor, ObservationNorm
+from ppo_pytorch.common.env_factory import NamedVecEnv, SimpleVecEnv, Monitor, ObservationNorm, ChannelTranspose
 from mlagents.envs.environment import UnityEnvironment, BrainInfo, BrainParameters, AllBrainInfo
 
 
 class UnityEnv(MultiplayerEnv):
-    def __init__(self, env_name, *args, **kwargs):
+    def __init__(self, env_name, visual_observations=False, *args, **kwargs):
+        self.visual_observations = visual_observations
         self.env = UnityEnvironment(env_name, *args, base_port=10000, **kwargs)
         brain = next(iter(self.env.reset().values()))
         assert self.env.number_external_brains == 1
         brain_params = self.env.external_brains[self.env.external_brain_names[0]]
-        self.observation_space = self._get_observation_space(brain_params)
-        self.action_space = self._get_action_space(brain_params)
+        self.observation_space = self._get_observation_space(brain_params, brain)
+        self.action_space = self._get_action_space(brain_params, brain)
         self.reward_range = (-1, 1)
         super().__init__(len(brain.agents))
 
@@ -30,7 +31,7 @@ class UnityEnv(MultiplayerEnv):
 
     def reset(self, *args, **kwargs):
         state = self.env.reset(*args, **kwargs)
-        return self._process_brain_info(state)
+        return self._process_brain_info(state)[0]
 
     def __enter__(self):
         return self
@@ -41,15 +42,19 @@ class UnityEnv(MultiplayerEnv):
     def _process_brain_info(self, env_state: AllBrainInfo):
         assert len(env_state) == 1, env_state
         brain: BrainInfo = next(iter(env_state.values()))
-        states = [self._process_state(s) for s in brain.vector_observations]
+        obs = brain.visual_observations[0] if self.visual_observations else brain.vector_observations
+        states = [self._process_state(s if self.visual_observations else s) for s in obs]
         return states, brain.rewards, brain.local_done, [{}] * self.num_players
 
-    def _get_action_space(self, brain_params: BrainParameters):
+    def _get_action_space(self, brain_params: BrainParameters, info: BrainInfo):
         type, size = brain_params.vector_action_space_type, brain_params.vector_action_space_size
         return self._create_space(type, size)
 
-    def _get_observation_space(self, brain_params: BrainParameters):
-        return self._create_space('continuous', [brain_params.vector_observation_space_size * brain_params.num_stacked_vector_observations])
+    def _get_observation_space(self, brain_params: BrainParameters, info: BrainInfo):
+        high = np.ones(np.shape(info.visual_observations)[-3:]) \
+            if self.visual_observations \
+            else np.ones(brain_params.vector_observation_space_size * brain_params.num_stacked_vector_observations)
+        return gym.spaces.Box(-high, high)
 
     def _process_action(self, action):
         return action
@@ -70,8 +75,9 @@ class UnityEnv(MultiplayerEnv):
 
 
 class UnityVecEnv(NamedVecEnv):
-    def __init__(self, env_path, observation_norm=False, parallel='thread'):
+    def __init__(self, env_path, parallel='thread', visual_observations=False, observation_norm=False):
         self.env_path = env_path
+        self.visual_observations = visual_observations
         env_name = os.path.basename(os.path.split(os.path.normpath(env_path))[0])
         self.observation_norm = observation_norm
         self.env_name = env_name
@@ -87,20 +93,22 @@ class UnityVecEnv(NamedVecEnv):
         env.close()
 
     def get_env_factory(self):
-        def make(env_path, observation_norm):
+        def make(env_path, observation_norm, visual_observations):
             while True:
                 worker_id = random.randrange(50000)
                 try:
-                    env = UnityEnv(env_path, worker_id=worker_id)
+                    env = UnityEnv(env_path, worker_id=worker_id, visual_observations=visual_observations)
                     break
                 except UnityWorkerInUseException:
                     print(f'Worker {worker_id} already in use')
             env = Monitor(env)
+            if self.visual_observations:
+                env = ChannelTranspose(env)
             if observation_norm:
                 env = ObservationNorm(env)
             return env
 
-        return partial(make, self.env_path, self.observation_norm)
+        return partial(make, self.env_path, self.observation_norm, self.visual_observations)
 
     def set_num_envs(self, num_envs):
         assert num_envs % self.num_players == 0, (num_envs, self.num_players)
@@ -114,13 +122,11 @@ class UnityVecEnv(NamedVecEnv):
 
     def step(self, actions):
         actions = np.split(np.asarray(actions), len(self.envs))
-        return self._cat_data(env.step(a) for env, a in zip(self.envs, actions))
+        data = [env.step(a) for env, a in zip(self.envs, actions)]
+        return [np.concatenate(x, 0) for x in zip(*data)]
 
     def reset(self):
-        return self._cat_data(env.reset() for env in self.envs)[0]
-
-    def _cat_data(self, data):
-        return [np.concatenate(x, 0) for x in zip(*data)]
+        return np.concatenate([env.reset() for env in self.envs], 0)
 
     # def __enter__(self):
     #     self.pool.__enter__()
